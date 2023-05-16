a2ae8a0227367e4af2cdb0d46ca5cd17278e7387
ayush <ayush.chauhan0008@gmail.com>
2023-05-17 00:10:56 +0530

    removed useCallback from App.jsx

--- src/hooks/http-firebase.jsx
+++ src/hooks/http-firebase.jsx
@@ -1,37 +1,30 @@
 import { useState, useCallback } from "react"
 
-function useHttp(applyData) {
+function useHttp() {
 	const [isLoading, setIsLoading] = useState(false)
 	const [error, setError] = useState(null)
 
-	const sendRequest = useCallback(async (requestConfig) => {
+	const sendRequest = useCallback(async (requestConfig, applyData) => {
 		setIsLoading(true)
 		setError(null)
 		try {
-			const response = await fetch(
-				// dynamic url and method configuration depending upon props sent by each component
-				// built to be re-usable for various requests like GET, POST etc.
-				requestConfig.url,
-				{
-                    // only append to the request if not empty i.e while fetching
-					method: requestConfig.method ? requestConfig.method : "GET",
-					headers: requestConfig.headers ? requestConfig.headers : {},
-					body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
-				}
-			)
+			const response = await fetch(requestConfig.url, {
+				method: requestConfig.method ? requestConfig.method : "GET",
+				headers: requestConfig.headers ? requestConfig.headers : {},
+				body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+			})
 
 			if (!response.ok) {
 				throw new Error("Request failed!")
 			}
 
 			const data = await response.json()
-			// appending the response data from the url to received applyData
 			applyData(data)
 		} catch (err) {
 			setError(err.message || "Something went wrong!")
 		}
 		setIsLoading(false)
-	}, [applyData])
+	}, [])
 
 	return {
 		isLoading,
